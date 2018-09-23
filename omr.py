63b186808b7bacc06a8bce2396b8bc047e6ce5c1
Parth Agarwal <iparthagarwal@gmail.com>
2018-09-24 00:38:14 +0530

    Add files via upload

---
+++ omr.py
@@ -0,0 +1,90 @@
+from scan import scan
+import cv2
+import numpy as np
+from imutils.contours import sort_contours
+
+# I/O
+
+image = cv2.imread(input('Enter the image name/path: '))
+original_image = image.copy()
+answer_key = {0 : 0, 1 : 1, 2 : 2, 3 : 3, 4 : 4}
+
+# Convert image to top-down view
+
+image = scan(image)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+_, thresholded_image = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+
+(_, contours, _) = cv2.findContours(thresholded_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+bubble_contours = []
+
+
+for contour in contours:
+
+    (x, y, w, h) = cv2.boundingRect(contour)
+
+    aspect_ratio = float(w) / h
+
+    if w >= 20 and h >= 20 and aspect_ratio >= 0.85 and aspect_ratio <= 1.15:
+        bubble_contours.append(contour)
+
+bubble_contours, _ = sort_contours(bubble_contours, method = 'top-to-bottom')
+
+# Number of correct answers
+score = 0
+
+no_of_options = int(input('Enter the number of options per question: '))
+
+for i in np.arange(0, len(bubble_contours), no_of_options):
+
+    row_contours, _ = sort_contours(bubble_contours[i : i + no_of_options])
+
+    bubbled = None
+
+    for (j, contour) in enumerate(row_contours):
+
+        mask = np.zeros(thresholded_image.shape, dtype = 'uint8')
+
+        # Draw option's contour
+        cv2.drawContours(mask, [contour], -1, 255, -1)
+
+        # Take only one option from the thresholded image
+        mask = cv2.bitwise_and(thresholded_image, thresholded_image, mask = mask)
+
+        # Find number of pixels filled/white/marked in option
+        no_of_pixels_active = cv2.countNonZero(mask)
+
+        # Store filled bubble's filled area and index
+        if bubbled is None or no_of_pixels_active > bubbled[0]:
+            bubbled = (no_of_pixels_active, j)
+
+    # Colour red for incorrect answer
+    colour = (0, 0, 255)
+    if bubbled[1] == answer_key[i // no_of_options]:
+
+        # Colour green for correct answer
+        colour = (0, 255, 0)
+        score += 1
+
+    cv2.drawContours(image, [row_contours[answer_key[i // no_of_options]]], -1, colour, 2)
+
+percentage = (score / float(len(bubble_contours) / no_of_options)) * 100
+
+cv2.putText(image, "{:.2f}%".format(percentage), (10, 10), cv2.FONT_HERSHEY_PLAIN, 0.9, (255, 0, 0), 2)
+
+cv2.imshow('Original', original_image)
+cv2.imshow('Sheet', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
--- scan.py
+++ scan.py
@@ -3,51 +3,67 @@ from skimage.filters import threshold_local
 from transform import four_point_transform
 import imutils
 
-image = cv2.imread(input('Enter image name/path: '))
-original_image = image.copy()
 
-# Decrease height for faster processing
-height_for_conversion = min(1000, image.shape[0])
-conversion_ratio = image.shape[0] / height_for_conversion
-image = imutils.resize(image, height = height_for_conversion)
 
-# Edge Detection
+def apply_filter(image, block_size = 11, offset = 10, method = 'gaussian'):
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 3, 75, 75)  # Tune parameters for better processing
-edged = cv2.Canny(gray, 75, 200)
+    # Apply the 'black and white' effect
 
-cv2.imshow('Edged', imutils.resize(edged, height = 800))
-cv2.waitKey(0)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = (image > threshold_local(image, block_size, offset = offset, method = method)). \
+                       astype("uint8") * 255
+    return image
 
-# Find contours
-(_, contours, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key = cv2.contourArea, reverse = True)[:min(3, len(contours))]
 
-flag = 0
+def scan(image):
 
-for contour in contours:
+    # Decrease height for faster processing
 
-    perimeter = cv2.arcLength(contour, True)
-    polygon = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+    height_for_conversion = min(1000, image.shape[0])
+    conversion_ratio = image.shape[0] / height_for_conversion
+    image = imutils.resize(image, height = height_for_conversion)
 
-    if len(polygon) == 4:
-        flag = 1
-        break
+    # Edge Detection
 
-if flag == 0:
-    print("No contour found with 4 points")
-    exit(1)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 3, 75, 75)  # Tune parameters for better processing
+    edged = cv2.Canny(gray, 75, 200)
 
-# Change image perspective
-warped_image = four_point_transform(original_image, polygon.reshape(4, 2) * conversion_ratio)
+    # Find contours
 
-# Apply the 'black and white' effect
-warped_image = cv2.cvtColor(warped_image, cv2.COLOR_BGR2GRAY)
-warped_image = (warped_image > threshold_local(warped_image, 11, offset = 10, method = "gaussian")).\
-                astype("uint8") * 255
+    (_, contours, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:min(3, len(contours))]
 
-cv2.imshow('Original', imutils.resize(original_image, height = height_for_conversion))
-cv2.imshow('Transformed', imutils.resize(warped_image, height = height_for_conversion))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    flag = 0
+
+    for contour in contours:
+
+        perimeter = cv2.arcLength(contour, True)
+        polygon = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+
+        if len(polygon) == 4:
+            flag = 1
+            break
+
+    if flag == 0:
+        print("No contour found with 4 points")
+        exit(1)
+
+    # Change image perspective
+
+    warped_image = four_point_transform(image, polygon.reshape(4, 2) * conversion_ratio)
+
+    return warped_image
+
+if __name__ == '__main__':
+    image = cv2.imread(input('Enter image name/path: '))
+    original_image = image.copy()
+
+    warped_image = scan(image)
+
+    warped_image = apply_filter(warped_image)
+
+    cv2.imshow('Original', imutils.resize(original_image, height = 650))
+    cv2.imshow('Transformed', imutils.resize(warped_image, height = 650))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
